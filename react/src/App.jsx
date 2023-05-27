3a1fbb1ef94a3a4eab8f211435c192c7bd42d459
burs <oum.burs9mry@gmail.com>
2023-05-27 09:49:33 +0300

    Initial commit

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+
+            <h1 className="text-center">Vite + React for Moe-website</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
