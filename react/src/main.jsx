3b60eb94635c33146766815a6c2f7fc8214d8c47
hailedereje <hailedereje88@gmail.com>
2023-06-01 06:54:33 +0300

    main

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import router from './router.jsx'
 import { RouterProvider } from 'react-router-dom';
-import { Slider } from './views/slider/Slider';
-import Navbar from './views/navigations/header';
-import TopNavBar from './views/navigations/top-navbar';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
