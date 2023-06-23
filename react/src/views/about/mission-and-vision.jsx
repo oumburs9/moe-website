d279d4c589a3d53b281b171334e68c4e9057c793
hailedereje <hailedereje88@gmail.com>
2023-06-23 14:35:12 +0300

    updating listing

--- react/src/views/about/mission-and-vision.jsx
+++ react/src/views/about/mission-and-vision.jsx
@@ -1,6 +1,16 @@
 import Side from '../../components/side.component'
 
-function Mission(){
+const ListItem = ({content})=>{
+  return (
+    <>
+    <li class="flex items-center">
+      <svg class="w-4 h-4 mr-1.5 text-green-500 dark:text-green-400 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+      {content}
+    </li>
+    </>
+  )
+}
+function Mission() {
   return (
     <>
       <nav>
@@ -21,45 +31,40 @@ function Mission(){
             <div className="">
               <p className="mt-[1.5em]">
                 <h4 className="text-lg"><strong>Our Vision</strong></h4>
-                Sustainably building an education and training system that ensures quality and 
-                equitable education for all citizens and that continuously produces a competent 
+                Sustainably building an education and training system that ensures quality and
+                equitable education for all citizens and that continuously produces a competent
                 and competitive workforce fueling the country’s economic development. <br />
               </p>
               <p className="mt-[2em]">
                 <h4 className="text-lg"><strong>Our Mission</strong></h4>
-                Ensuring effective, quality and equitable education and training system through 
-                building the implementation capacity of the education sector at all levels, designing 
-                and regulating standards of efficiency, expanding standardized education throughout the country, 
-                as well as complementing and leveraging education sector development interventions with strategic 
+                Ensuring effective, quality and equitable education and training system through
+                building the implementation capacity of the education sector at all levels, designing
+                and regulating standards of efficiency, expanding standardized education throughout the country,
+                as well as complementing and leveraging education sector development interventions with strategic
                 communications and public awareness.
               </p>
               <p className="mt-[2em]">
                 <h4 className="text-lg"><strong>Our Values</strong></h4>
-                <div className="ml-4 grid grid-cols-3 gap-10">
-                  <ul className="list-disc">
-                    <li>Effectiveness and Efficiency</li>
-                    <li>Quality</li>
-                    <li>Equity</li>
-                    <li>Participatory</li>
-                    <li>Exemplary</li>
+                <div className="m-4 grid grid-cols-3 gap-10">
+
+                  {/* <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Password requirements:</h2> */}
+                  <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">   
+                      {['Effectiveness and Efficiency','Quality','Equity','Participatory','Exemplary'].map(c => <ListItem content={c}/>)}
                   </ul>
-                  <ul className="list-disc">
-                    <li>Commitment</li>
-                    <li>Excellence</li>
-                    <li>Professionalism</li>
-                    <li>Change/innovation</li>
-                    <li>Compassion</li>
+
+                  <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                  {['Commitment','Quality','Excellence','Professionalism','Change/innovation','Compassion'].map(c => <ListItem content={c}/>)}
                   </ul>
                 </div>
-              </p>    
+              </p>
             </div>
           </div>
         </div>
         <div className="md:col-span-1 pl-4">
-          <Side/>
+          <Side />
         </div>
       </div>
-     
+
     </>
   )
 }
