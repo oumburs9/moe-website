0735232a4cac1ea87ee6968796b94c060be15032
hailedereje <hailedereje88@gmail.com>
2023-06-23 14:14:41 +0300

    updating contactus page

--- react/src/views/contact-us/contact-us.jsx
+++ react/src/views/contact-us/contact-us.jsx
@@ -16,10 +16,10 @@ export const ContactUs = () => {
                 <iframe style={{ height: "70vh", width: "100%", border: 0, padding: '10px' }} src="https://www.google.com/maps/embed/v1/place?q=Ministry+of+Education,+Constitution+Road,+Bridgetown,+Barbados&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
 
                 <div className="grid md:grid-cols-12 pt-5">
-                    <div className="md:col-span-9">
+                    <div className="md:col-span-8">
                         <ContactForm />
                     </div>
-                    <div className="md:col-span-3">
+                    <div className="md:col-span-4">
                         <div className="flex flex-col gap-3 p-2">
                             <div className='mx-auto mb-3 contact-icon w-[70px] h-[70px] rounded-full flex items-center justify-center'>
                                 <img src={images.World} alt="" className='w-[70%] h-[70%]' />
@@ -43,31 +43,37 @@ export const ContactUs = () => {
 
 export const ContactForm = () => {
     return (
-        <div className="">
+        <div className="flex flex-col pl-5">
             <div className="p-3 flex">
-                <p className="text-[50px] text-blue-600 capitalize">
+                <p className="text-[45px] text-blue-600 capitalize font-bold ">
                     Contact us
                 </p>
             </div>
-            <form action="" className="flex flex-col p-2 gap-3">
-                <div className="flex flex-col p-2 items-start gap-2">
-                    <label className="text-sm capitalize text-gray-500 font-bold">your name</label>
-                    <input type="text" name="" id="" className="w-[90%] hover:shadow focus:shadow bg-gray-100" /></div>
-                <div className="flex flex-col p-2 items-start gap-2">
-                    <label className="text-sm capitalize text-gray-500 font-bold">your email</label>
-                    <input type="email" name="" id="" className="w-[90%] hover:shadow focus:shadow bg-gray-100" /></div>
-                <div className="flex flex-col p-2 items-start gap-2">
-                    <label className="text-sm capitalize text-gray-500 font-bold">subject</label>
-                    <input type="text" name="" id="" className="w-[90%] hover:shadow focus:shadow bg-gray-100" /></div>
-                <div className="flex flex-col p-2 items-start gap-2">
-                    <label className="text-sm capitalize text-gray-500 font-bold">message</label>
-                    <textarea rows={5} name="" id="" className="w-[90%] hover:shadow focus:shadow bg-gray-100" /></div>
-                <div className="flex justify-end mr-24">
-                    <button className="btn-color-gb py-3 px-4 text-white font-bold text-sm flex items-center uppercase hover:shadow">
-                        <p>Send message</p>
-                    </button>
-                </div>
-            </form>
+            
+<form className='flex flex-col pl-5 w-[60%]'>
+  <div class="mb-6">
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">FullName</label>
+    <input type="text" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="yourName" required/>
+  </div>
+  <div class="mb-6">
+    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+    <input type="email" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required/>
+  </div>
+  <div class="mb-6">
+    <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
+    <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required/>
+  </div>
+ 
+  <div className="mb-6">
+    
+<label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+<textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+  </div>
+  
+  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">SEND</button>
+</form>
+
         </div>
     )
 }
