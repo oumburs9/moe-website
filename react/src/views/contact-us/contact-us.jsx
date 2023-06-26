085e08d994967ec1420f4e8ad74af6182dd7aaa1
ysihak-B <ysihakbazezew@gmail.com>
2023-06-26 17:16:08 +0300

    updating contacts page

--- react/src/views/contact-us/contact-us.jsx
+++ react/src/views/contact-us/contact-us.jsx
@@ -1,5 +1,6 @@
 import { images } from '../../assets'
 import Breadcrumb from '../../components/breadcrumb.component'
+import Titles from '../../components/title.component'
 export const ContactUs = () => {
 
     const path = [{name: 'Home', link: ''},{name: 'Contact Us'}]
@@ -12,12 +13,14 @@ export const ContactUs = () => {
             <div className="flex flex-col mt-4">
 
                 <iframe style={{ height: "70vh", width: "100%", border: 0, padding: '10px' }} src="https://www.google.com/maps/embed/v1/place?q=Ministry+of+Education,+Constitution+Road,+Bridgetown,+Barbados&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
-
-                <div className="grid md:grid-cols-12 pt-5">
-                    <div className="md:col-span-8">
+                <div className='p-10 pb-0'>
+                    <Titles title={'Contact Us'} subtitle={'You can share us your thoughts and suggestions'}/>
+                </div>
+                <div className="grid md:grid-cols-3 p-10">
+                    <div className="md:col-span-2">
                         <ContactForm />
                     </div>
-                    <div className="md:col-span-4">
+                    <div className="md:col-span-1">
                         <div className="flex flex-col gap-3 p-2">
                             <div className='mb-3 contact-icon w-[60px] h-[60px] rounded-full flex items-center justify-center'>
                                 <img src={images.World} alt="" className='w-[70%] h-[70%]' />
@@ -41,37 +44,26 @@ export const ContactUs = () => {
 
 export const ContactForm = () => {
     return (
-        <div className="flex flex-col pl-5">
-            <div className="p-3 flex">
-                <p className="text-[45px] text-blue-600 capitalize font-bold ">
-                    Contact us
-                </p>
-            </div>
-
-            <form className='flex flex-col pl-5 w-[60%]'>
+        <div className="">
+            <form className='flex flex-col p-5 w-[80%]'>
                 <div class="mb-6">
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">FullName</label>
-                    <input type="text" id="email" class="shadow-sm bg-gray-50 border border-green-800 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light " placeholder="yourName" required />
+                    <label for="email" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">FullName</label>
+                    <input type="text" id="email" className="shadow-lg border border-gray-300 p-4  text-gray-900 text-sm rounded-lg focus:outline-none focus:shadow-sm hover:shadow-sm block w-full" placeholder="Your Name" required />
                 </div>
                 <div class="mb-6">
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                    <input type="email" id="password" class="shadow-sm bg-gray-50 border border-green-800 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                    <label for="email" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Email</label>
+                    <input type="text" id="email" className="shadow-lg border border-gray-300 p-4  text-gray-900 text-sm rounded-lg focus:outline-none focus:shadow-sm hover:shadow-sm block w-full" placeholder="Your Email" required />
                 </div>
                 <div class="mb-6">
-                    <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"></label>
-                    <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-green-800 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+                    <label for="email" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Subject</label>
+                    <input type="text" id="email" className="shadow-lg border border-gray-300 p-4  text-gray-900 text-sm rounded-lg focus:outline-none focus:shadow-sm hover:shadow-sm block w-full" placeholder="subject" required />
                 </div>
-
                 <div className="mb-6">
-
                     <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-                    <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-green-800 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-
+                    <textarea id="message" rows="4" className="shadow-lg border border-gray-300 p-4  text-gray-900 text-sm rounded-lg focus:outline-none focus:shadow-sm hover:shadow-sm block w-full" placeholder="Write your thoughts here..."></textarea>
                 </div>
-
-                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">SEND</button>
+                <button type="submit" class="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">SEND</button>
             </form>
-
         </div>
     )
 }
