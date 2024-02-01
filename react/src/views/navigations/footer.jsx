9991a14b8374abf9c77db68bcc84155774f676f1
jabiroumer <jabiroumer@blackhilltechnologies.com>
2024-02-01 16:02:17 +0300

    fixing the logo

--- react/src/views/navigations/footer.jsx
+++ react/src/views/navigations/footer.jsx
@@ -71,9 +71,9 @@ const Footer = () => {
                   <p className="text-gray-500">Monday to Friday <br/> 2:30 - 11:00 local time</p>
 
                   {/* Assuming the user is not authenticated */}
-                  <a href="/contact-us" class="grid grid-cols-1 mt-5">
+                  <a href="/contact-us" className="grid grid-cols-1 mt-5">
                                 <button type="button" id="submitsubscribe" name="send"
-                                    class="py-3 btn bg-[#2048A1] hover:bg-[#2048A1] border-[#2048A1] hover:border-[#2048A1] text-white rounded-xl shadow-lg hover:shadow-[#2048A1]">
+                                    className="py-3 btn bg-[#2048A1] hover:bg-[#2048A1] border-[#2048A1] hover:border-[#2048A1] text-white rounded-xl shadow-lg hover:shadow-[#2048A1]">
                                     Contact Us
                                 </button>
                     </a>
@@ -137,45 +137,45 @@ const SocialLinks = () => {
       </li>
       {/* Add similar code for other social links */}
       {/* {{-- Telegram --}} */}
-    <li class="inline hover:-translate-y-1 transition-all saturate-50 hover:saturate-100">
+    <li className="inline hover:-translate-y-1 transition-all saturate-50 hover:saturate-100">
         <a href="#" target="_blank"
-            class="btn btn-icon btn-sm rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
+            className="btn btn-icon btn-sm rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
             <img src="https://cdn-icons-png.flaticon.com/128/5968/5968804.png"
                 data-src="https://cdn-icons-png.flaticon.com/128/5968/5968804.png" alt="Telegram " title="Telegram "
-                width="64" height="64" class="lzy lazyload--done w-7"
-                srcset="https://cdn-icons-png.flaticon.com/128/5968/5968804.png 4x"/>
+                width="64" height="64" className="lzy lazyload--done w-7"
+                srcSet="https://cdn-icons-png.flaticon.com/128/5968/5968804.png 4x"/>
         </a>
     </li>
 
     {/* {{-- Youtube --}} */}
-    <li class="inline hover:-translate-y-1 transition-all saturate-50 hover:saturate-100">
-        <a href="#" target="_blank" class="">
+    <li className="inline hover:-translate-y-1 transition-all saturate-50 hover:saturate-100">
+        <a href="#" target="_blank" className="">
             <img src="https://cdn-icons-png.flaticon.com/128/1384/1384060.png"
                 data-src="https://cdn-icons-png.flaticon.com/128/1384/1384060.png" alt="Youtube " title="Youtube "
-                width="64" height="64" class="lzy lazyload--done w-7"
-                srcset="https://cdn-icons-png.flaticon.com/128/1384/1384060.png 4x"/>
+                width="64" height="64" className="lzy lazyload--done w-7"
+                srcSet="https://cdn-icons-png.flaticon.com/128/1384/1384060.png 4x"/>
         </a>
     </li>
 
     {/* {{-- Tiktok --}} */}
-    <li class="inline hover:-translate-y-1 transition-all saturate-50 hover:saturate-100">
+    <li className="inline hover:-translate-y-1 transition-all saturate-50 hover:saturate-100">
         <a href="#" target="_blank"
-            class="btn btn-icon btn-sm rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
+            className="btn btn-icon btn-sm rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
             <img src="https://cdn-icons-png.flaticon.com/128/2504/2504942.png"
                 data-src="https://cdn-icons-png.flaticon.com/128/2504/2504942.png" alt="Tiktok " title="Tiktok "
-                width="64" height="64" class="lzy lazyload--done w-7"
-                srcset="https://cdn-icons-png.flaticon.com/128/2504/2504942.png 4x"/>
+                width="64" height="64" className="lzy lazyload--done w-7"
+                srcSet="https://cdn-icons-png.flaticon.com/128/2504/2504942.png 4x"/>
         </a>
     </li>
 
     {/* {{-- Twitter --}} */}
-    <li class="inline hover:-translate-y-1 transition-all saturate-50 hover:saturate-100">
+    <li className="inline hover:-translate-y-1 transition-all saturate-50 hover:saturate-100">
         <a href="#" target="_blank"
-            class="btn btn-icon btn-sm rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
+            className="btn btn-icon btn-sm rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 dark:hover:bg-indigo-600">
             <img src="https://cdn-icons-png.flaticon.com/128/733/733579.png"
                 data-src="https://cdn-icons-png.flaticon.com/128/733/733579.png" alt="Twitter " title="Twitter "
-                width="64" height="64" class="lzy lazyload--done w-7"
-                srcset="https://cdn-icons-png.flaticon.com/128/733/733579.png 4x"/>
+                width="64" height="64" className="lzy lazyload--done w-7"
+                srcSet="https://cdn-icons-png.flaticon.com/128/733/733579.png 4x"/>
         </a>
     </li>
     </ul>
